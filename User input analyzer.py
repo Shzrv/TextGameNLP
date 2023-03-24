f6b4f813664fc49578f4d48e54b016edbebe016e
mShak <m.shakh@posteo.de>
2023-03-24 15:43:01 +0100

    Fixed rule-based input analyzer prototype for directions

--- User input analyzer.py
+++ User input analyzer.py
@@ -3,37 +3,30 @@
 import nltk
 
 
-# 1. Define function that accepts user input, tokenizes it, tags it, and chunks it.
+# 1. Define function that accepts user input and tokenizes it.
 def input_process(cmd):
-    # tokenize the string - means split to words and other meaning fragments like periods, commas and n't
-    tokens = nltk.word_tokenize(cmd)
-
-    # tag the list of tokens with parts of speech
-    tagged = nltk.pos_tag(tokens)
-
-    # group the words into meaningful segments
-    chunked = nltk.chunk.ne_chunk(tagged)
-    return chunked
+    tokens = nltk.word_tokenize(cmd.lower())
+    return tokens
 
 
 # 2. define a function that checks if the command given is a move-command
 def check_move_direction(tokens, direction):
-    # check if this is a move command and return the input string for further processing
-    return "go" or "walk" or "move" in tokens and direction in tokens
+    return "go" in tokens and direction in tokens
 
 
-# 3. define a function that checks, what direction the user wants to move. Here, use a dictionary where one the values
-# are the commands, and the keys are lambda functions that make use of the function in 2.
-def determine_direction(direction):
+# 3. define a function that checks, what direction the user wants to move.
+def determine_direction(inpt):
     predefined_direction = {
         "left": lambda tokens: check_move_direction(tokens, "left"),
         "right": lambda tokens: check_move_direction(tokens, "right"),
         "forward": lambda tokens: check_move_direction(tokens, "forwards"),
-        "back": lambda tokens: check_move_direction(tokens, "backwards")
+        "back": lambda tokens: check_move_direction(tokens, "backwards"),
     }
+
     for cmd, check in predefined_direction.items():
-        if check(direction):
+        if check(inpt):
             return cmd
+
     return None
 
 
